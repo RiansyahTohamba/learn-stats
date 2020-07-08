6ea32f6dd587455a1bfd82256121d40b7d7922e6
muhammad riansyah <mriansyah93@gmail.com>
2020-07-08 17:39:48 +0700

    update model

--- sw-eng-estimation/main.py
+++ sw-eng-estimation/main.py
@@ -13,31 +13,51 @@ print(WorkProduct.ExternalRepresentation.screen)
 
 # step 2 :  Selecting project activities
 class ProjectActivities:
-#     sss
+    # PART 2 process
     task_sets
     def choose_framework(type_fw):
         pass
     
+class EffortModel:
+    # Section 33.7
+    def cocomo():
+        pass    
+
+    def cocomo_2():
+        pass
+        
 class MainEstimates:
     #step 3 : Predict the number of people who will be available to do the work is specified.
-    def predicting_staffing_levels:
+    def predicting_staffing_levels():
         number_of_available_people = 100
-#step 4: predicting_effort          
+        
+    #step 4: predicting_effort          
     def predicting_effort(models,size_work_products):
-#         Estimation tools use one or more models (Section 33.7) 
-        if models == 'cocomo':
-#         models relate the size of the project to the effort required.
+        #Estimation tools use one or more models (Section 33.7) 
+        if models == cocomo:
+        #models relate the size of the project to the effort required.
             if size_work_products.kloc > 100:
                 return str(10)+'person-month'
-        if models == 'cocomo II':
+        if models == cocomo_2:
+            return 100
     
+    # step 5 : Given the results of step 4, costs can be computed by allocating labor rates to the project activities noted in step 2. 
     def predicting_cost():
         import locale
         return locale.currency(188518982.18, grouping=True )
+
+    # step 6 :  Predicting software schedules. 
+    # When effort, staffing level, and project activities are known,
+    def allocating_labor_across_activities():
+        pass
     
-    def main:
+    def effort_distribution():
+        pass    
+
+    def predicting_schedule():
+        num_peoples = predicting_staffing_level
+        allocating_labor_across_activities(num_peoples,effort_distribution)   
+
+    def main():
         ProjectActivities.task_sets = ['planning','codings','deploy']
 
-# 5. Predicting software cost. Given the results of step 4, costs can be computed by allocating labor rates to the project activities noted in step 2. 
-
-# 6. Predicting software schedules. When effort, staffing level, and project activities are known,a draft schedule can be produced by allocating labor across software engineering activities based on recommended models for effort distribution discussed later in this chapter.
