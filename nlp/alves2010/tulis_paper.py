f44930dfd3b6c178c24554c8f0c9c21b822f9d12
muhammad riansyah <mriansyah93@gmail.com>
2021-01-08 10:12:46 +0700

    remove jupyter notebook from think-stats

--- nlp/alves2010/tulis_paper.py
+++ nlp/alves2010/tulis_paper.py
@@ -9,6 +9,7 @@ def ie_preprocess(document):
 # 3. part-of-speech tagger [3]:
     sentences = [nltk.pos_tag(sent) for sent in sentences]    
     return sentences
+    
 # sisa tahap 4 dan 5
 
 def ekspor():
@@ -23,7 +24,7 @@ def eksporTree():
 		output.write(str(nltk.ne_chunk(sent)))
 
 def ideComparative():
-	# ide comparative? JJR dan RBR
+	# ide comparative? 
 	# bagus nih,buat membandingkan a > b atau a < b
 	# 1. search tuple dengan value JJR OR RBR
 	# 2. 
